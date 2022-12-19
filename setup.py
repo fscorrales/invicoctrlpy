d1b0c9ae84ca20987c909e74008d843f903a83fa
Fernando Corrales <corrales_fernando@hotmail.com>
2022-12-19 17:04:46 -0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='invicoctrlpy',
+    description="""
+    Set of classes, methods and jupyter notebooks to control INVICO’s 
+    (Instituto de Vivienda de Corrientes) budget execution. The database 
+    it's not provide within this package due to privacy reasons.
+    """,
+    author='Fernando Corrales',
+    author_email='corrales_fernando@hotmail.com',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+    ],
+    dependency_links=['http://github.com/fscorrales/invicodatpy/tarball/master#egg=invicodatpy#']
+)
