b491f9a6673d269bc408cc1d08eb4b54553bac57
Fernando Corrales <corrales_fernando@hotmail.com>
2022-12-25 17:19:36 -0300

    feat: update sgf_vs_sscc class

--- src/invicoctrlpy/gastos/sgf_vs_sscc.py
+++ src/invicoctrlpy/gastos/sgf_vs_sscc.py
@@ -17,20 +17,17 @@ from dataclasses import dataclass, field
 
 import pandas as pd
 from datar import base, dplyr, f, tidyr
-from invicodatpy.sgf.resumen_rend_prov import ResumenRendProv
-from invicodatpy.sscc.all import BancoINVICO, CtasCtes
+
+from invicoctrlpy.utils.import_dataframe import ImportDataFrame
 import update_db
 
 
 @dataclass
 # --------------------------------------------------
-class SGFVsSSCC():
+class SGFVsSSCC(ImportDataFrame):
     ejercicio:str = str(dt.datetime.now().year)
     db_path:str = None
     update_db:bool = False
-    ctas_ctes:pd.DataFrame = field(init=False, repr=False)
-    sgf_resumen_rend:pd.DataFrame = field(init=False, repr=False)
-    sscc_banco_invico:pd.DataFrame = field(init=False, repr=False)
 
     # --------------------------------------------------
     def __post_init__(self):
@@ -39,31 +36,6 @@ class SGFVsSSCC():
         if self.update_db:
             self.update_sql_db()
         self.import_dfs()
-
-    # --------------------------------------------------
-    def get_outside_path(self):
-        dir_path = os.path.dirname(
-                        os.path.dirname(
-                            os.path.dirname(
-                                os.path.dirname(
-                                    os.path.dirname(
-                                        os.path.abspath(
-                                            inspect.getfile(
-                                                inspect.currentframe()))
-                        )))))
-        return dir_path
-
-    # --------------------------------------------------
-    def get_db_path(self):
-        self.db_path = (self.get_outside_path() 
-                        + '/Python Output/SQLite Files')
-        return self.db_path
-
-    # --------------------------------------------------
-    def get_update_path_input(self):
-        dir_path = (self.get_outside_path() 
-                    + '/invicoDB/Base de Datos')
-        return dir_path
 
     # --------------------------------------------------
     def update_sql_db(self):
@@ -84,55 +56,16 @@ class SGFVsSSCC():
     def import_dfs(self):
         self.import_ctas_ctes()
         self.import_banco_invico()
-        self.import_resumen_rend()
-
-    # --------------------------------------------------
-    def import_ctas_ctes(self):
-        df = CtasCtes().from_sql(self.db_path + '/sscc.sqlite') 
-        self.ctas_ctes = df
-        return self.ctas_ctes
+        self.import_resumen_rend(self.ejercicio)
 
     # --------------------------------------------------
     def import_banco_invico(self):
-        df = BancoINVICO().from_sql(self.db_path + '/sscc.sqlite')  
-        df = df.loc[df['ejercicio'] == self.ejercicio]
+        df = super().import_banco_invico(self.ejercicio)
         df = df.loc[df['movimiento'] != 'DEPOSITO']
         df['importe'] = df['importe'] * (-1)
         df.reset_index(drop=True, inplace=True)
-        map_to = self.ctas_ctes.loc[:,['map_to', 'sscc_cta_cte']]
-        df = pd.merge(
-            df, map_to, how='left',
-            left_on='cta_cte', right_on='sscc_cta_cte')
-        df['cta_cte'] = df['map_to']
-        df.drop(['map_to', 'sscc_cta_cte'], axis='columns', inplace=True)
         self.sscc_banco_invico = df
         return self.sscc_banco_invico
-
-    # --------------------------------------------------
-    def import_resumen_rend(self):
-        df = ResumenRendProv().from_sql(self.db_path + '/sgf.sqlite')  
-        df = df.loc[df['ejercicio'] == self.ejercicio]
-        df.reset_index(drop=True, inplace=True)
-        map_to = self.ctas_ctes.loc[:,['map_to', 'sgf_cta_cte']]
-        df = pd.merge(
-            df, map_to, how='left',
-            left_on='cta_cte', right_on='sgf_cta_cte')
-        df['cta_cte'] = df['map_to']
-        df.drop(['map_to', 'sgf_cta_cte'], axis='columns', inplace=True)
-        #Filtramos los registros duplicados en la 106
-        df_106 = df.copy()
-        df_106 = df_106 >> \
-            dplyr.filter_(f.cta_cte == '106') >> \
-            dplyr.distinct(
-                f.mes, f.fecha, f.beneficiario,
-                f.libramiento_sgf, 
-                _keep_all=True
-            )
-        df = df >> \
-            dplyr.filter_(f.cta_cte != '106') >> \
-            dplyr.bind_rows(df_106)
-        self.sgf_resumen_rend = pd.DataFrame(df)
-        return self.sgf_resumen_rend
 
     # --------------------------------------------------
     def control_mes_cta_cte(self):
@@ -172,9 +105,19 @@ class SGFVsSSCC():
     def control_completo(self):
         sscc_completo = self.sscc_banco_invico.copy()
         sscc_completo = sscc_completo >> \
+            dplyr.rename_with(lambda x: 'sscc_' + x) >> \
             dplyr.rename(
-                debitos_sscc = f.importe
+                sscc_debitos = f.sscc_importe,
+                ejercicio = f.sscc_ejercicio,
+                mes = f.sscc_mes,
+                fecha = f.sscc_fecha,
+                libramiento = f.sscc_libramiento,
+                cta_cte = f.sscc_cta_cte,
+                beneficiario = f.sscc_beneficiario,
             )
+            # dplyr.rename(
+            #     debitos_sscc = f.importe
+            # )
             # dplyr.select(
             #     f.mes, f.fecha, f.cta_cte, 
             #     f.libramiento, f.beneficiario, 
@@ -182,11 +125,20 @@ class SGFVsSSCC():
             # )
         sgf_completo = self.sgf_resumen_rend.copy()
         sgf_completo = sgf_completo >> \
+            dplyr.rename_with(lambda x: 'sgf_' + x) >> \
             dplyr.rename(
-                libramiento = f.libramiento_sgf,
-                neto_sgf = f.importe_neto, 
-                retenciones_sgf = f.retenciones,
+                libramiento = f.sgf_libramiento_sgf,
+                ejercicio = f.sgf_ejercicio,
+                mes = f.sgf_mes,
+                fecha = f.sgf_fecha,
+                cta_cte = f.sgf_cta_cte,
+                beneficiario = f.sgf_beneficiario,
             )
+            # dplyr.rename(
+            #     libramiento = f.libramiento_sgf,
+            #     neto_sgf = f.importe_neto, 
+            #     retenciones_sgf = f.retenciones,
+            # )
             # dplyr.select(
             #     f.mes, f.fecha, f.cta_cte, 
             #     f.beneficiario, 
@@ -200,7 +152,7 @@ class SGFVsSSCC():
                 dplyr.across(dplyr.where(base.is_numeric), tidyr.replace_na, 0)
             ) >> \
             dplyr.mutate(
-                diferencia = f.neto_sgf + f.retenciones_sgf - f.debitos_sscc
+                diferencia = f.sgf_importe_neto + f.sgf_retenciones - f.sscc_debitos
             )
         control_completo.sort_values(
             by=['mes', 'fecha','cta_cte', 'beneficiario'], 
