1b0bd1935d773400ed4b9e2fb779ce1602d0f803
Fernando Corrales <fscorrales@gmail.com>
2022-12-27 07:02:09 -0300

    fix: relocate methods in import_dataframe.py

--- src/invicoctrlpy/utils/import_dataframe.py
+++ src/invicoctrlpy/utils/import_dataframe.py
@@ -20,29 +20,20 @@ class ImportDataFrame(HanglingPath):
     icaro:pd.DataFrame = field(init=False, repr=False)
     icaro_neto_rdeu:pd.DataFrame = field(init=False, repr=False)
     siif_rdeu012:pd.DataFrame = field(init=False, repr=False)
-    sgf_resumen_rend_cuit:pd.DataFrame = field(init=False, repr=False)
-    sgf_resumen_rend:pd.DataFrame = field(init=False, repr=False)
-    sscc_banco_invico:pd.DataFrame = field(init=False, repr=False)
     siif_rf602:pd.DataFrame = field(init=False, repr=False)
     siif_rfondo07tp:pd.DataFrame = field(init=False, repr=False)
     siif_comprobantes:pd.DataFrame = field(init=False, repr=False)
     siif_rci02:pd.DataFrame = field(init=False, repr=False)
+    sgf_resumen_rend_cuit:pd.DataFrame = field(init=False, repr=False)
+    sgf_resumen_rend:pd.DataFrame = field(init=False, repr=False)
+    sscc_banco_invico:pd.DataFrame = field(init=False, repr=False)
+
 
     # --------------------------------------------------
     def import_ctas_ctes(self) -> pd.DataFrame:
         df = CtasCtes().from_sql(self.db_path + '/sscc.sqlite') 
         self.ctas_ctes = df
         return self.ctas_ctes
-
-    # --------------------------------------------------
-    def import_siif_rdeu012(self, ejercicio:str = None) -> pd.DataFrame:
-        df = DeudaFlotanteRdeu012().from_sql(self.db_path + '/siif.sqlite')
-        if ejercicio != None:
-            df = df.loc[df['ejercicio'] == ejercicio]
-        # No estoy seguro del orden Desc o Asc
-        df.sort_values(by=['fecha_hasta'], inplace=True, ascending=False)
-        self.siif_rdeu012 = df
-        return self.siif_rdeu012
 
     # --------------------------------------------------
     def import_icaro(self, ejercicio:str = None) -> pd.DataFrame:
@@ -59,33 +50,6 @@ class ImportDataFrame(HanglingPath):
         df.drop(['map_to', 'icaro_cta_cte'], axis='columns', inplace=True)
         self.icaro = df
         return self.icaro
-
-    # --------------------------------------------------
-    def import_resumen_rend_cuit(self, ejercicio:str = None) -> pd.DataFrame:
-        df = JoinResumenRendProvCuit().from_sql(self.db_path + '/sgf.sqlite')  
-        if ejercicio != None:
-            df = df.loc[df['ejercicio'] == ejercicio]
-        df.reset_index(drop=True, inplace=True)
-        map_to = self.ctas_ctes.loc[:,['map_to', 'sgf_cta_cte']]
-        df = pd.merge(
-            df, map_to, how='left',
-            left_on='cta_cte', right_on='sgf_cta_cte')
-        df['cta_cte'] = df['map_to']
-        df.drop(['map_to', 'sgf_cta_cte'], axis='columns', inplace=True)
-        #Filtramos los registros duplicados en la 106
-        df_106 = df.copy()
-        df_106 = df_106 >> \
-            dplyr.filter_(f.cta_cte == '106') >> \
-            dplyr.distinct(
-                f.mes, f.fecha, f.beneficiario,
-                f.libramiento_sgf, 
-                _keep_all=True
-            )
-        df = df >> \
-            dplyr.filter_(f.cta_cte != '106') >> \
-            dplyr.bind_rows(df_106)
-        self.sgf_resumen_rend_cuit = pd.DataFrame(df)
-        return self.sgf_resumen_rend_cuit
 
     # --------------------------------------------------
     def import_icaro_neto_rdeu(self, ejercicio:str) -> pd.DataFrame:
@@ -153,46 +117,14 @@ class ImportDataFrame(HanglingPath):
         return self.icaro_neto_rdeu
 
     # --------------------------------------------------
-    def import_banco_invico(self, ejercicio:str = None):
-        df = BancoINVICO().from_sql(self.db_path + '/sscc.sqlite')
-        if ejercicio != None:  
-            df = df.loc[df['ejercicio'] == ejercicio]
-        df.reset_index(drop=True, inplace=True)
-        map_to = self.ctas_ctes.loc[:,['map_to', 'sscc_cta_cte']]
-        df = pd.merge(
-            df, map_to, how='left',
-            left_on='cta_cte', right_on='sscc_cta_cte')
-        df['cta_cte'] = df['map_to']
-        df.drop(['map_to', 'sscc_cta_cte'], axis='columns', inplace=True)
-        self.sscc_banco_invico = df
-        return self.sscc_banco_invico
-
-    # --------------------------------------------------
-    def import_resumen_rend(self, ejercicio:str = None):
-        df = ResumenRendProv().from_sql(self.db_path + '/sgf.sqlite')  
+    def import_siif_rdeu012(self, ejercicio:str = None) -> pd.DataFrame:
+        df = DeudaFlotanteRdeu012().from_sql(self.db_path + '/siif.sqlite')
         if ejercicio != None:
             df = df.loc[df['ejercicio'] == ejercicio]
-        df.reset_index(drop=True, inplace=True)
-        map_to = self.ctas_ctes.loc[:,['map_to', 'sgf_cta_cte']]
-        df = pd.merge(
-            df, map_to, how='left',
-            left_on='cta_cte', right_on='sgf_cta_cte')
-        df['cta_cte'] = df['map_to']
-        df.drop(['map_to', 'sgf_cta_cte'], axis='columns', inplace=True)
-        #Filtramos los registros duplicados en la 106
-        df_106 = df.copy()
-        df_106 = df_106 >> \
-            dplyr.filter_(f.cta_cte == '106') >> \
-            dplyr.distinct(
-                f.mes, f.fecha, f.beneficiario,
-                f.libramiento_sgf, 
-                _keep_all=True
-            )
-        df = df >> \
-            dplyr.filter_(f.cta_cte != '106') >> \
-            dplyr.bind_rows(df_106)
-        self.sgf_resumen_rend = pd.DataFrame(df)
-        return self.sgf_resumen_rend
+        # No estoy seguro del orden Desc o Asc
+        df.sort_values(by=['fecha_hasta'], inplace=True, ascending=False)
+        self.siif_rdeu012 = df
+        return self.siif_rdeu012
 
     # --------------------------------------------------
     def import_siif_rf602(self, ejercicio:str = None):
@@ -233,3 +165,72 @@ class ImportDataFrame(HanglingPath):
             df = df.loc[df['ejercicio'] == self.ejercicio]
         self.siif_rci02 = df
         return self.siif_rci02
+
+    # --------------------------------------------------
+    def import_resumen_rend(self, ejercicio:str = None):
+        df = ResumenRendProv().from_sql(self.db_path + '/sgf.sqlite')  
+        if ejercicio != None:
+            df = df.loc[df['ejercicio'] == ejercicio]
+        df.reset_index(drop=True, inplace=True)
+        map_to = self.ctas_ctes.loc[:,['map_to', 'sgf_cta_cte']]
+        df = pd.merge(
+            df, map_to, how='left',
+            left_on='cta_cte', right_on='sgf_cta_cte')
+        df['cta_cte'] = df['map_to']
+        df.drop(['map_to', 'sgf_cta_cte'], axis='columns', inplace=True)
+        #Filtramos los registros duplicados en la 106
+        df_106 = df.copy()
+        df_106 = df_106 >> \
+            dplyr.filter_(f.cta_cte == '106') >> \
+            dplyr.distinct(
+                f.mes, f.fecha, f.beneficiario,
+                f.libramiento_sgf, 
+                _keep_all=True
+            )
+        df = df >> \
+            dplyr.filter_(f.cta_cte != '106') >> \
+            dplyr.bind_rows(df_106)
+        self.sgf_resumen_rend = pd.DataFrame(df)
+        return self.sgf_resumen_rend
+
+    # --------------------------------------------------
+    def import_resumen_rend_cuit(self, ejercicio:str = None) -> pd.DataFrame:
+        df = JoinResumenRendProvCuit().from_sql(self.db_path + '/sgf.sqlite')  
+        if ejercicio != None:
+            df = df.loc[df['ejercicio'] == ejercicio]
+        df.reset_index(drop=True, inplace=True)
+        map_to = self.ctas_ctes.loc[:,['map_to', 'sgf_cta_cte']]
+        df = pd.merge(
+            df, map_to, how='left',
+            left_on='cta_cte', right_on='sgf_cta_cte')
+        df['cta_cte'] = df['map_to']
+        df.drop(['map_to', 'sgf_cta_cte'], axis='columns', inplace=True)
+        #Filtramos los registros duplicados en la 106
+        df_106 = df.copy()
+        df_106 = df_106 >> \
+            dplyr.filter_(f.cta_cte == '106') >> \
+            dplyr.distinct(
+                f.mes, f.fecha, f.beneficiario,
+                f.libramiento_sgf, 
+                _keep_all=True
+            )
+        df = df >> \
+            dplyr.filter_(f.cta_cte != '106') >> \
+            dplyr.bind_rows(df_106)
+        self.sgf_resumen_rend_cuit = pd.DataFrame(df)
+        return self.sgf_resumen_rend_cuit
+
+    # --------------------------------------------------
+    def import_banco_invico(self, ejercicio:str = None):
+        df = BancoINVICO().from_sql(self.db_path + '/sscc.sqlite')
+        if ejercicio != None:  
+            df = df.loc[df['ejercicio'] == ejercicio]
+        df.reset_index(drop=True, inplace=True)
+        map_to = self.ctas_ctes.loc[:,['map_to', 'sscc_cta_cte']]
+        df = pd.merge(
+            df, map_to, how='left',
+            left_on='cta_cte', right_on='sscc_cta_cte')
+        df['cta_cte'] = df['map_to']
+        df.drop(['map_to', 'sscc_cta_cte'], axis='columns', inplace=True)
+        self.sscc_banco_invico = df
+        return self.sscc_banco_invico
