c16c526bbac2e92016b55d1aba69919d8181bfa9
Fernando Corrales <corrales_fernando@hotmail.com>
2023-01-16 20:59:51 -0300

    feat: add control mensual to control_honorarios

--- src/invicoctrlpy/gastos/control_honorarios/control_honorarios.py
+++ src/invicoctrlpy/gastos/control_honorarios/control_honorarios.py
@@ -74,41 +74,83 @@ class ControlHonorarios(ImportDataFrame):
         df = super().import_slave(ejercicio = self.ejercicio)
         cta_cte = self.import_siif_rcg01_uejp(self.ejercicio)
         cta_cte = cta_cte.loc[:, ['nro_comprobante', 'cta_cte']]
-        df = df.merge(self.siif_rcg01_uejp, on='nro_comprobante', how='left')
+        df = df.merge(cta_cte, on='nro_comprobante', how='left')
         self.slave = df
         return self.slave
 
-    # # --------------------------------------------------
-    # def control_mes(self):
-    #     siif_haberes_mes = self.siif_comprobantes_haberes_neto_rdeu.copy()
-    #     siif_haberes_mes = siif_haberes_mes >> \
-    #         dplyr.select(f.mes, f.importe) >> \
-    #         dplyr.group_by(f.mes) >> \
-    #         dplyr.summarise(ejecutado_siif = base.sum_(f.importe),
-    #                         _groups = 'drop')
-    #     sscc_mes = self.sscc_banco_invico.copy()
-    #     sscc_mes = sscc_mes >> \
-    #         dplyr.select(
-    #             f.mes, f.importe
-    #         ) >> \
-    #         dplyr.group_by(f.mes) >> \
-    #         dplyr.summarise(
-    #             pagado_sscc = base.sum_(f.importe),
-    #             _groups = 'drop')
-    #     control_mes = siif_haberes_mes >> \
-    #         dplyr.full_join(sscc_mes) >> \
-    #         dplyr.mutate(
-    #             dplyr.across(dplyr.where(base.is_numeric), tidyr.replace_na, 0)
-    #         ) >> \
-    #         dplyr.mutate(
-    #             diferencia = f.ejecutado_siif- f.pagado_sscc
-    #         )
-    #     #     dplyr.filter_(~dplyr.near(f.diferencia, 0))
-    #     control_mes.sort_values(by=['mes'], inplace= True)
-    #     control_mes = pd.DataFrame(control_mes)
-    #     control_mes['dif_acum'] = control_mes['diferencia'].cumsum()
-    #     control_mes.reset_index(drop=True, inplace=True)
-    #     return control_mes
+    # --------------------------------------------------
+    def control_mes(self):
+        slave_mes = self.slave.copy()
+        slave_mes = slave_mes >> \
+            dplyr.select(f.mes, f.cta_cte, f.importe_bruto) >> \
+            dplyr.group_by(f.mes, f.cta_cte) >> \
+            dplyr.summarise(ejecutado_slave = base.sum_(f.importe_bruto),
+                            _groups = 'drop')
+        sgf_mes = self.sgf_resumen_rend_honorarios.copy()
+        sgf_mes = sgf_mes >> \
+            dplyr.select(
+                f.mes, f.cta_cte, f.importe_bruto
+            ) >> \
+            dplyr.group_by(f.mes, f.cta_cte) >> \
+            dplyr.summarise(
+                pagado_sgf = base.sum_(f.importe_bruto),
+                _groups = 'drop')
+        control_mes = slave_mes >> \
+            dplyr.full_join(sgf_mes) >> \
+            dplyr.mutate(
+                dplyr.across(dplyr.where(base.is_numeric), tidyr.replace_na, 0)
+            ) >> \
+            dplyr.mutate(
+                diferencia = f.ejecutado_slave - f.pagado_sgf
+            )
+        #     dplyr.filter_(~dplyr.near(f.diferencia, 0))
+        control_mes.sort_values(by=['mes', 'cta_cte'], inplace= True)
+        control_mes = pd.DataFrame(control_mes)
+        control_mes['dif_acum'] = control_mes['diferencia'].cumsum()
+        control_mes.reset_index(drop=True, inplace=True)
+        return control_mes
+
+    # --------------------------------------------------
+    def import_slave(self):
+        df = super().import_slave(ejercicio = self.ejercicio)
+        cta_cte = self.import_siif_rcg01_uejp(self.ejercicio)
+        cta_cte = cta_cte.loc[:, ['nro_comprobante', 'cta_cte']]
+        df = df.merge(cta_cte, on='nro_comprobante', how='left')
+        self.slave = df
+        return self.slave
+
+    # --------------------------------------------------
+    def control_mes_beneficiario(self):
+        slave_mes = self.slave.copy()
+        slave_mes = slave_mes >> \
+            dplyr.select(f.mes, f.cta_cte, f.razon_social, f.importe_bruto) >> \
+            dplyr.rename(beneficiario = f.razon_social) >> \
+            dplyr.group_by(f.mes, f.cta_cte, f.beneficiario) >> \
+            dplyr.summarise(ejecutado_slave = base.sum_(f.importe_bruto),
+                            _groups = 'drop')
+        sgf_mes = self.sgf_resumen_rend_honorarios.copy()
+        sgf_mes = sgf_mes >> \
+            dplyr.select(
+                f.mes, f.cta_cte, f.beneficiario, f.importe_bruto
+            ) >> \
+            dplyr.group_by(f.mes, f.cta_cte, f.beneficiario) >> \
+            dplyr.summarise(
+                pagado_sgf = base.sum_(f.importe_bruto),
+                _groups = 'drop')
+        control_mes = slave_mes >> \
+            dplyr.full_join(sgf_mes) >> \
+            dplyr.mutate(
+                dplyr.across(dplyr.where(base.is_numeric), tidyr.replace_na, 0)
+            ) >> \
+            dplyr.mutate(
+                diferencia = f.ejecutado_slave - f.pagado_sgf
+            )
+        #     dplyr.filter_(~dplyr.near(f.diferencia, 0))
+        control_mes.sort_values(by=['mes', 'cta_cte', 'beneficiario'], inplace= True)
+        control_mes = pd.DataFrame(control_mes)
+        control_mes['dif_acum'] = control_mes['diferencia'].cumsum()
+        control_mes.reset_index(drop=True, inplace=True)
+        return control_mes
 
     # # --------------------------------------------------
     # def control_completo(self):
